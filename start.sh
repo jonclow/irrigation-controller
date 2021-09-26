d57721b2cc83d46741dae28f222136bb4ad4270f
Jonathan Clow <jonclow@redmercury.co.nz>
2021-09-26 21:33:49 +1300

    update start script

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+arch=$(uname -m)
+
+echo "The system arch is $arch"
+
+if [ "$arch" = "armv7l" ]
+then
+  cd ~/irrigation-controller || exit
+fi
+
 npm install && npm run build && npm run start
