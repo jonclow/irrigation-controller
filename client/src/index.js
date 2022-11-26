3fb3f832db8858e13a60111960f0437479ce8983
Jonathan Clow <jonclow@redmercury.co.nz>
2022-11-26 21:58:50 +1300

    upgrade to React 18

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render } from "react-dom";
 import './index.css';
 import App from './components/App'
+import { createRoot } from 'react-dom/client';
 
-render(<App />, document.getElementById('root'));
+createRoot(document.getElementById('root')).render(<App tab="home" />);
